8dec7079804c326c4eae01f4932264c80bcb09f5
Patrick Du <PDu@fgfbrands.com>
2019-03-01 14:25:38 -0500

    changed architecture

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,20 +1,59 @@
 import React from 'react';
-import PageTitle from '../layout/PageTitle';
-import Introduction from './Introduction';
-import AboutBox from './AboutBox';
 
 const About = () => {
     return (
         <div className="container-fluid main-px mt-5">
-            <Introduction title="Nice to meet you" subtitle="I'm Patrick Du" paragraph1=" I'm currently wIn my free time, I can be found at the gym playing volleyball, working out, or drinking bubble tea." />
-            <AboutBox title="" subtitle="Education" paragraph1="I am studying Kinesiology at the University of Waterloo. Being in the applied health sciences faculty, I am constantly surrounded by " />
-            <AboutBox
-                title="Career aspirations"
-                subtitle="Exploring the link betweeen fitness and technology"
-                paragraph1="Attending a school at the forefront of innovation I am constantly surrounded by hard working and talented individuals that push me in whatever it is that I do.  "
-                paragraph2="In my first year, whenever I was asked about my future plans I immediately blurted out that I would be a physiotherapist. It was the easiest answer. What else would you be doing in the kinesiology program otherwise?" />
-        </div>
+            <div className="row">
+                <div className="col">
+                    <h5 className="title-font med no-y-space">Nice to meet you</h5>
+                    <h3 className="p-font bold display-4 ">Hi, I'm Patrick</h3>
+                    <p className="s-font reg">I'm a 2nd year student currently working at FGF Brands as a front-end developer. I attend the University of Waterloo for Kinesiology. In my free time, I play sports, work out, browse reddit, and drink bubble tea. </p>
+                </div>
+                <div className="col">
+                    {/*image*/}
+                </div>
+            </div>
+
+            <h1 className="p-font bold text-center mt-5">Hobbies</h1>
+
+            <div className="row">
+                    <div className="aboutDiscover mx-auto"></div>
+                    <div className="aboutDiscover mx-auto"></div>
+            </div>
+
+            <h1 className="p-font bold text-center mt-5">Discover a little more about me!</h1>
+
+            <div className="row">
+                    <div className="aboutDiscover mx-auto"></div>
+                    <div className="aboutDiscover mx-auto"></div>
+            </div>
+
+            {/* <div className="aboutBox my-5 p-3 text-center">
+                <p className="title-font med no-y-space">My Story</p>
+                <h3 className="p-font med">How I got into Coding</h3>
+                <hr/>
+                <p className="s-font-reg">Whenever I was asked about my future plans I immediately blurted out that I would be a physiotherapist. What else would you be doing in kinesiology otherwise? I mean besides applying for a prestigious medical school, becoming a chiropractor, personal trainer, or everyone’s favourite - a gym teacher.</p>
+                <p className="s-font-reg">I portrayed myself as a confident and prepared student, when in reality I really had no idea what I wanted to do. On the other hand, all my friends seemed to have a clear-cut idea of what they wanted to do, the clubs they would join for experience, the clinics they would volunteer at, or the hackathons they would attend.</p>
+                <p className="s-font-reg">Attending a school at the forefront of innovation means that I am constantly surrounded by studious individuals. Whether it was during the easy going summer term or the weeks leading up to finals, I constantly found friends working on side projects.  Being a student in kinesiology there aren’t many opportunities for side projects. My days would generally consist of studying, working out, and drinking protein shakes as you would expect.</p>
+                <p className="s-font-reg">This was no different in the summer except there was no studying. I dedicated that chunk of time for studying to playing video games and rewatching every single Marvel movies leading up to Avengers: Infinity War - all 18 of them. In the same span of time, my roommate had just completed 2 coding courses and was designing the architecture of his second website. His work inspired me to create my own website so based on his recommendation I took a beginner HTML5 and CSS3. At the end of the summer, I launched my first website.</p>
+                <p className="s-font-reg">Following this project, my interest in coding had been sparked and with coop applications coming up I decided to take a stab at applying for a position I was in no way qualified for. With a combination of hard-work and coffee I was able to land an interview and secure a web developer position job at FGF Brands for my first coop term. Since then, I have attended several hackathons and expanded my skill set immensely.</p>
+            </div>
+
+            <div className="aboutBox my-5 p-3 text-center">
+                <p className="title-font med no-y-space">Career Aspirations</p>
+                <h2 className="p-font med ">What about my kinesiology degree?</h2>
+                <hr />
+                <p className="s-font-reg">For the future, I plan to complete my kinesiology degree with a health informatics option expanding my understanding of the field that investigates how computer technologies can be utilized to better generate, manage, store, and apply health information.  </p>
+                <h5 className="s-font med">My work aims to explore the intersection of technology with fitness and health.</h5> */}
+
+        </div >
+
+
+
+
+
     )
+
 }
 
 export default About;
