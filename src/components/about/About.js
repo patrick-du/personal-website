ef47a98f9664c90e2db66c5629755397873f02e6
Patrick Du <PDu@fgfbrands.com>
2019-03-01 15:04:06 -0500

    about page

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -4,34 +4,28 @@ const About = () => {
     return (
         <div className="container-fluid main-px mt-5">
             <div className="row">
-                <div className="col">
+                <div className="col-9">
                     <h5 className="title-font med no-y-space">Nice to meet you</h5>
-                    <h3 className="p-font bold display-4 ">Hi, I'm Patrick</h3>
-                    <p className="s-font reg">I'm a 2nd year student currently working at FGF Brands as a front-end developer. I attend the University of Waterloo for Kinesiology. In my free time, I play sports, work out, browse reddit, and drink bubble tea. </p>
+                    <h3 className="p-font bold display-4 ">I'm Patrick</h3>
+                    <p className="s-font reg">I'm a 2nd year student currently working at FGF Brands as a front-end developer. <br /> I attend the <span className="med">University of Waterloo</span> for Kinesiology. <br />In my free time, I play sports, work out, browse reddit, and drink bubble tea. </p>
                 </div>
-                <div className="col">
-                    {/*image*/}
+                <div className="col-3">
+                    <div className="circle float-right"></div>{/*image*/}
                 </div>
             </div>
 
-            <h1 className="p-font bold text-center mt-5">Hobbies</h1>
 
-            <div className="row">
-                    <div className="aboutDiscover mx-auto"></div>
-                    <div className="aboutDiscover mx-auto"></div>
-            </div>
 
             <h1 className="p-font bold text-center mt-5">Discover a little more about me!</h1>
 
             <div className="row">
-                    <div className="aboutDiscover mx-auto"></div>
-                    <div className="aboutDiscover mx-auto"></div>
+                
             </div>
 
-            {/* <div className="aboutBox my-5 p-3 text-center">
+            <div className="aboutBox my-5 p-3 text-center">
                 <p className="title-font med no-y-space">My Story</p>
                 <h3 className="p-font med">How I got into Coding</h3>
-                <hr/>
+                <hr />
                 <p className="s-font-reg">Whenever I was asked about my future plans I immediately blurted out that I would be a physiotherapist. What else would you be doing in kinesiology otherwise? I mean besides applying for a prestigious medical school, becoming a chiropractor, personal trainer, or everyone’s favourite - a gym teacher.</p>
                 <p className="s-font-reg">I portrayed myself as a confident and prepared student, when in reality I really had no idea what I wanted to do. On the other hand, all my friends seemed to have a clear-cut idea of what they wanted to do, the clubs they would join for experience, the clinics they would volunteer at, or the hackathons they would attend.</p>
                 <p className="s-font-reg">Attending a school at the forefront of innovation means that I am constantly surrounded by studious individuals. Whether it was during the easy going summer term or the weeks leading up to finals, I constantly found friends working on side projects.  Being a student in kinesiology there aren’t many opportunities for side projects. My days would generally consist of studying, working out, and drinking protein shakes as you would expect.</p>
@@ -39,13 +33,19 @@ const About = () => {
                 <p className="s-font-reg">Following this project, my interest in coding had been sparked and with coop applications coming up I decided to take a stab at applying for a position I was in no way qualified for. With a combination of hard-work and coffee I was able to land an interview and secure a web developer position job at FGF Brands for my first coop term. Since then, I have attended several hackathons and expanded my skill set immensely.</p>
             </div>
 
-            <div className="aboutBox my-5 p-3 text-center">
-                <p className="title-font med no-y-space">Career Aspirations</p>
-                <h2 className="p-font med ">What about my kinesiology degree?</h2>
+            <div className="aboutBox my-5 p-3 ">
+                <p className="title-font med no-y-space text-center">Career Aspirations</p>
+                <h2 className="p-font med text-center">What about my kinesiology degree?</h2>
                 <hr />
                 <p className="s-font-reg">For the future, I plan to complete my kinesiology degree with a health informatics option expanding my understanding of the field that investigates how computer technologies can be utilized to better generate, manage, store, and apply health information.  </p>
-                <h5 className="s-font med">My work aims to explore the intersection of technology with fitness and health.</h5> */}
+                <h5 className="s-font lead text-center">My work strives to explore the intersection of technology with fitness and health.</h5>
+                <p className="s-font-reg">By studying Kinesiology I will acquire a profound understanding of physiological, biomechanical, and psychological dynamic principles and mechanisms of movement allowing me to innovate technology for the fitness and health industry in ways that a computer science or engineering graduate cannot.</p>
+                <blockquote className="blockquote">
+                    <p className="mb-0">I think the biggest innovations of the 21st century will be at the intersection of biology and technology. A new era is beginning.</p>
+                    <footer className="blockquote-footer">Steve Jobs in <cite title="Source Title">Source Title</cite></footer>
+                </blockquote>
 
+            </div>
         </div >
 
 
