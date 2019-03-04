214fb7e1d9ff089a130289138831042504b3d57e
Patrick Du <PDu@fgfbrands.com>
2019-03-04 16:28:17 -0500

    fixed stylign

--- src/components/work/WorkBox.js
+++ src/components/work/WorkBox.js
@@ -5,18 +5,18 @@ const WorkBox = (props) => {
     return (
         <div className="row py-5">
             <div className="col">
-                <div className="customCard">
+                <div style ={styles.customCard}>
                     <CardContent title="Huddle" body="A modern sports betting application that connects fans while supporting local venues." imgPath="huddle.jpg" />
                 </div>
             </div>
             <div className="col">
-                <div className="customCard mx-auto">
+                <div className="mx-auto" style={styles.customCard}>
                     <CardContent title="Huddle" body="A modern sports betting application that connects fans while supporting local venues." imgPath="huddle.jpg" />
                 </div>
 
             </div>
             <div className="col">
-                <div className="customCard float-right">
+                <div className="float-right" style={styles.customCard}>
                     <CardContent title="Huddle" body="A modern sports betting application that connects fans while supporting local venues." imgPath="huddle.jpg" />
                 </div>
 
@@ -27,19 +27,12 @@ const WorkBox = (props) => {
 
 const styles = {
     customCard: {
-
+        width: '80%',
+        height: '500px',
+        boxShadow: '0 0 10px #ccc',
+        padding: '5rem 4rem 5rem 4rem',
+        borderRadius: '15px'
     }
-
-    /* Card */
-.customCard {
-    width: 80%;
-    height: 500px;
-    box-shadow: 0 0 10px #ccc;
-    padding: 5rem 4rem 5rem 4rem;
-    border-radius: 15px;
-  }
-  
-  
 }
 
 export default WorkBox;
