77f4f6bc5388f3aadf494cd59f63914d0d2a5a9a
Patrick Du <PDu@fgfbrands.com>
2019-03-04 10:31:01 -0500

    quote for story

--- src/components/layout/PicHolder.js
+++ src/components/layout/PicHolder.js
@@ -4,7 +4,7 @@ const PicHolder = (props) => {
     const { imgPath, caption } = props;
     return (
             <figure className="figure my-4 px-5 py-3">
-                <img src={require(`../../assets/${imgPath}`)} className="figure-img img-fluid rounded" alt=""/>
+                <img src={require(`../../${imgPath}`)} className="figure-img img-fluid rounded" alt=""/>
                     <figcaption className="figure-caption text-center">{caption}</figcaption>
             </figure>
     );
