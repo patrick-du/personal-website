c61bbc8b5ebe8e0787bf0538629fc38ffdfc2b54
Patrick Du <PDu@fgfbrands.com>
2019-03-05 17:17:36 -0500

    work

--- src/components/work/CardContent.js
+++ src/components/work/CardContent.js
@@ -4,22 +4,36 @@ const Card = (props) => {
     const { title, body, imgPath, badge1, badge2, badge3 } = props;
 
     return (
-        <React.Fragment>
-            <div className="p-font bold" style={styles.title}>{title}</div>
-            <div className="s-font med" style={styles.body}>{body}</div>
-            <br></br>
-            <span class="badge badge-pill s-font" style={styles.badge}>{badge1}</span> &nbsp;
-            <span class="badge badge-pill s-font" style={styles.badge}>{badge2}</span> &nbsp;
-            <span class="badge badge-pill s-font" style={styles.badge}>{badge3}</span>
+        // <div style={styles.customCard}>
+        //     <div className="p-font bold" style={styles.title}>{title}</div>
+        //     <div className="s-font med" style={styles.body}>{body}</div>
+
+        //     <div className="">
+        //         <span class="badge badge-pill s-font" style={styles.badge}>{badge1}</span> &nbsp;
+        //         <span class="badge badge-pill s-font" style={styles.badge}>{badge2}</span> &nbsp;
+        //         <span class="badge badge-pill s-font" style={styles.badge}>{badge3}</span>
+        //     </div>
+        // </div>
+            /*<img src={require(`../../assets/${imgPath}`)} />*/
+            <div className="card" style={styles.custom}>
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
 
 
-            
-          {/*<img src={require(`../../assets/${imgPath}`)} />*/}  
-        </React.Fragment>
     )
 }
 
 const styles = {
+    custom: {
+        width: '80%',
+        height: '500px',
+        borderRadius: '15px',
+        boxShadow: '0 0 10px #ccc'
+    },
     title: {
         fontSize: '2rem'
     },
@@ -28,6 +42,9 @@ const styles = {
         fontSize: '15px'
     },
 
+    icon: {
+        fontSize: '30px'
+    },
     badge: {
         border: '1px solid #5368fc',
         backgroundColor: '#',
@@ -36,7 +53,11 @@ const styles = {
 
     img: {
 
-    }
+    },
+  
+
+
+
 }
 
 
---
+++ src/components/work/Huddle.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Huddle = () => {
+    return (
+        <div className="container-fluid main-px mt-5">
+        
+
+            
+        </div>
+    )
+
+}
+
+const styles = {
+    
+}
+
+export default Huddle;
