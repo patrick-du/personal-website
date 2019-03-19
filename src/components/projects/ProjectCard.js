78f0c0132264c667ff361f8cc230886bd7307001
Patrick Du <PDu@fgfbrands.com>
2019-03-19 17:17:44 -0400

    projCard

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -2,25 +2,35 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const ProjectCard = (props) => {
-    const { link, imgPath, projName, projDesc, projType, backgroundColor } = props;
+    const { link, imgPath, projName, projDesc, projWhat, projType, backgroundColor } = props;
     return (
         <Link to={link} className="linkdec">
             <div className="projectCard">
-                <div className="imgRow text-center" style={{ backgroundColor: backgroundColor }}>
-                    <img src={require(`../../assets/${imgPath}`)} alt="" className="projectCardImage p-3" />
+
+                <div id="under768">
+                    <div className="imgRow text-center" style={{ backgroundColor: backgroundColor }}>
+                        <img src={require(`../../assets/${imgPath}`)} alt="" className="projectCardImage p-3" />
+                    </div>
+                    <div className="projectCardText p-3">
+                        <p className="p-font med card-title" id="projName">{projName}</p>
+                        <p className="s-font lightText card-text" id="projDesc">{projDesc}<div id="projType">{projType}</div></p>
+                    </div>
                 </div>
 
-                <div className="row" >
+                <div id="above768">
                     <div id="projCol1" className="text-center" style={{ backgroundColor: backgroundColor }}>
                         <img src={require(`../../assets/${imgPath}`)} alt="" className="projectCardImage p-3" />
                     </div>
                     <div id="projCol2" style={{ padding: '1rem' }}>
                         <div className="projectCardText px-0">
                             <p className="p-font med card-title" id="projName">{projName}</p>
+                            <p className="s-font card-text" id="projWhat">{projWhat}</p>
                             <p className="s-font lightText card-text" id="projDesc">{projDesc}<div id="projType">{projType}</div></p>
                         </div>
                     </div>
                 </div>
+
+
             </div>
         </Link >
     )
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -6,9 +6,9 @@ const Projects = () => {
     return (
         <div className="container-fluid container-padding">
             <PageTitle pageName="Selected Projects" descriptionBottom="Side projects and hackathon submissions" />
-            <ProjectCard link="/projects/huddle" backgroundColor="#ea9345" imgPath="huddle.png" projDesc="React Native, Node JS" projName="Huddle" projType="Side Project" />
-            <ProjectCard link="/projects/personalwebsite" backgroundColor="brown" imgPath="huddle.png" projDesc="React" projName="Personal Website" projType="Side Project" />
-            <ProjectCard link="/projects/fitbud" backgroundColor="brown" imgPath="fitbud.png" projDesc="React, Node JS, Express JS" projName="FitBud" projType="DeltaHacks V" />
+            <ProjectCard link="/projects/huddle" backgroundColor="#55efc4" imgPath="huddle.png" projDesc="React Native • Node JS" projWhat="Modern sports betting mobile app" projName="Huddle" projType="Side Project" />
+            <ProjectCard link="/projects/personalwebsite" backgroundColor="#ff7675" imgPath="huddle.png" projDesc="React" projWhat="Portfolio to showcase my projects and work " projName="Personal Website" projType="Side Project" />
+            <ProjectCard link="/projects/fitbud" backgroundColor="#74b9ff" imgPath="fitbud.png" projDesc="React • Node JS • Express JS" projWhat="All-in-one fitness app" projName=" FitBud" projType="DeltaHacks V" />
         </div>
     );
 }
