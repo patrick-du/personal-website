348eae26292700564b0668a2eb5c8beefe5d9c6b
patrick-du <patrickduu@gmail.com>
2019-04-05 10:19:15 -0400

    added some pictures to projects

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -20,22 +20,22 @@ const Projects = () => {
 
             </div>
             <div className="fade-in-top-about-1">
-                <ProjectCard link="/projects/huddle" backgroundColor="#55efc4" imgPath="Untitled.png" projDesc="React Native • Node JS" projWhat="Modern sports betting mobile app" projName="Huddle" projType="Side Project" />
+                <ProjectCard link="/projects/huddle" backgroundColor="#55efc4" imgPath="Weatherly.png" projDesc="React Native • Node JS" projWhat="Modern sports betting mobile app" projName="Huddle" projType="Side Project" />
 
             </div>
 
             <div className="fade-in-top-about-1">
-                <ProjectCard link="/projects/weatherly" backgroundColor="#a29bfe" imgPath="Weatherly.png" projDesc="Vanilla JS • HTML • CSS" projWhat="Weather forecast progressive web app" projName="Weatherly" projType="Side Project" />
+                <ProjectCard link="/projects/weatherly" backgroundColor="#ebd0f4" imgPath="Weatherly.png" projDesc="Vanilla JS • HTML • CSS" projWhat="Weather forecast progressive web app" projName="Weatherly" projType="Side Project" />
 
             </div>
 
             <div className="fade-in-top-about-1">
-                <ProjectCard link="/projects/personalwebsite" backgroundColor="#ff7675" imgPath="huddle.png" projDesc="React" projWhat="Portfolio to showcase my projects and work " projName="Personal Website" projType="Side Project" />
+                <ProjectCard link="/projects/personalwebsite" backgroundColor="#ff7675" imgPath="Weatherly.png" projDesc="React" projWhat="Portfolio to showcase my projects and work " projName="Personal Website" projType="Side Project" />
 
             </div>
 
             <div className="fade-in-top-about-1">
-                <ProjectCard link="/projects/fitbud" backgroundColor="#ffab7e" imgPath="FitBudLogo.png" projDesc="React • Node JS • Express JS" projWhat="All-in-one fitness app" projName=" FitBud" projType="uOttaHack 2019" />
+                <ProjectCard link="/projects/fitbud" backgroundColor="#ffc8a2" imgPath="FitBud.png" projDesc="React • Node JS • Express JS" projWhat="All-in-one fitness app" projName=" FitBud" projType="uOttaHack 2019" />
 
             </div>
 
